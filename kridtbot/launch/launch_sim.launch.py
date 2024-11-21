91069d6f168bc6861ecbfb96561829ca6d1c6b59
Crelle490 <christian.clt@gmail.com>
2024-11-21 12:06:14 +0100

    g

--- kridtbot/launch/launch_sim.launch.py
+++ kridtbot/launch/launch_sim.launch.py
@@ -39,7 +39,7 @@ def generate_launch_description():
     # Launch Gazebo with World
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('sros_gz_sim'), 'launch', 'gz_sim.launch.py'
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
                 )]),launch_arguments={'gz_args': ['-r -v4', world], 'on_exit_shutdown': 'true'}.items()
              )
 
