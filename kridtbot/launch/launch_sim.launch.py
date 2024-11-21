ef4d80311acbe38f867dc625469831383c56ce24
nich1157 <nickfc1@hotmail.com>
2024-11-21 11:34:46 +0100

    w

--- kridtbot/launch/launch_sim.launch.py
+++ kridtbot/launch/launch_sim.launch.py
@@ -7,8 +7,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -18,26 +19,35 @@ def generate_launch_description():
 
     package_name='kridtbot' 
 
+    #### Launch 1: Launch the robot state publisher
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    #### Launch 2: Launch Gazebo and run
+    # Find world file
+    default_world = os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world')
+
+    world = LaunchConfiguration('world')
+    world_arg = DeclareLaunchArgument('world', default_value=default_world, description='Gazebo world file')
+
+    # Launch Gazebo with World
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-                )]),launch_arguments={'gz_args': }.items()
+                )]),launch_arguments={'gz_args': ['-r ']}.items() # -r run sim, supply path to world file
              )
 
+    #### Launch 3: Spawn the robot in Gazebo
     spawn_entity = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_spawn_model.launch.py'
                 )]),launch_arguments={'world':'empty', 'topic':'robot_description', 'name': 'kridt_bot', 'x': '5.0', 'y': '5.0', 'z': '0.5'}.items()
              )
 
-    # Launch them all!
+    # Launch all
     return LaunchDescription([
         rsp,
         gazebo,
