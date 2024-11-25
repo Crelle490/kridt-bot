c78332df4a5f99e374df0700bb83b2b6f9a4653f
Crelle490 <christian.clt@gmail.com>
2024-11-25 12:20:32 +0100

    g

--- kridtbot/launch/launch_sim.launch.py
+++ kridtbot/launch/launch_sim.launch.py
@@ -26,32 +26,35 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-
-
-
     #### Launch 2: Launch Gazebo and run
     # Find world file
-    default_world = os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world')
-
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'empty.world'
+        )    
+    
     world = LaunchConfiguration('world')
-    world_arg = DeclareLaunchArgument('world', default_value=default_world, description='Gazebo world file')
 
-    # Launch Gazebo with World
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-                )]),launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
-
-
-
-    #### Launch 3: Spawn the robot in Gazebo
-    spawn_entity = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_spawn_model.launch.py'
-                )]),launch_arguments={'topic':'robot_description', 'name': 'kridt_bot', 'x': '5.0', 'y': '5.0', 'z': '0.5'}.items()
-             )
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'my_bot',
+                                   '-z', '0.3'],
+                        output='screen')
 
     # Launch all
     return LaunchDescription([
